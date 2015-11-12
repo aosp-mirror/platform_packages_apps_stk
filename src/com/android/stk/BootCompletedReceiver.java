3f2bd962a7e8c94966a629271f59c88b9ecc1006
Teruaki Minami <teruaki.x.minami@sonymobile.com>
2015-11-12 17:41:13 +0100

    Null check action of intents in StkCmdReceiver and BootCompletedReceiver
    These commands would cause NullPointerException:
    adb shell am broadcast -n com.android.stk/.BootCompletedReceiver
    adb shell am broadcast -n com.android.stk/.StkCmdReceiver
    
    (Alternative would be to set exported=false for these receivers.)
    
    Change-Id: Id6ae11a86d9653b5c546e3c3c5421b4ec6b95d4e

--- src/com/android/stk/BootCompletedReceiver.java
+++ src/com/android/stk/BootCompletedReceiver.java
@@ -35,6 +35,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
+        if (action == null) {
+            return;
+        }
 
         // make sure the app icon is removed every time the device boots.
         if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
