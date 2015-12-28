2ba20ccc8a92d118ac2b82aa39c9b194859f9539
Ryuto Sawada <ryuto.x.sawada@sonymobile.com>
2015-12-28 17:10:35 +0100

    Empty toast should not be displayed
    Empty toast is displayed when empty alpha identifier is specified.
    If msg.txt is null, it should not be displayed.
    
    Change-Id: I212e922a0bc4a07a8598607e320c317f86eeece3

--- src/com/android/stk/StkAppService.java
+++ src/com/android/stk/StkAppService.java
@@ -1416,7 +1416,7 @@ public class StkAppService extends Service implements Runnable {
     }
 
     private void launchEventMessage(int slotId, TextMessage msg) {
-        if (msg == null || (msg.text != null && msg.text.length() == 0)) {
+        if (msg == null || msg.text == null || (msg.text != null && msg.text.length() == 0)) {
             CatLog.d(LOG_TAG, "launchEventMessage return");
             return;
         }
