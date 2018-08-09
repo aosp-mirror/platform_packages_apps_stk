03a79d9ec1c3ace1cec0507bf3184e3757dbe933
Yoshiaki Naka <yoshiaki.naka@sony.com>
2018-08-09 18:41:31 +0900

    Fix incorrect handling of pending activity and state management
    StkMenuActivity for secondary menu and StkInputActivity currently
    register theirselves as pending activity, but it is never unregistered
    by theirselves. Pending activity should be unregistered when it is
    brought back to the foreground unless terminal response has already been
    sent to the card.
    
    Bug: 113367485
    Test: Manually confirmed that the pending activity works as expected.
    
    Change-Id: I335d1867c121191b31d8436eea4982a7d45a1c90

--- src/com/android/stk/StkAppService.java
+++ src/com/android/stk/StkAppService.java
@@ -635,11 +635,13 @@ public class StkAppService extends Service implements Runnable {
             case OP_SET_ACT_INST:
                 Activity act = (Activity) msg.obj;
                 if (mStkContext[slotId].mActivityInstance != act) {
-                    CatLog.d(LOG_TAG, "Set activity instance - " + act);
+                    CatLog.d(LOG_TAG, "Set pending activity instance - " + act);
                     Activity previous = mStkContext[slotId].mActivityInstance;
                     mStkContext[slotId].mActivityInstance = act;
-                    // Finish the previous one if it has not been finished yet somehow.
-                    if (previous != null && !previous.isDestroyed() && !previous.isFinishing()) {
+                    // Finish the previous one if it was replaced with new one
+                    // but it has not been finished yet somehow.
+                    if (act != null && previous != null && !previous.isDestroyed()
+                            && !previous.isFinishing()) {
                         CatLog.d(LOG_TAG, "Finish the previous pending activity - " + previous);
                         previous.finish();
                     }
--- src/com/android/stk/StkInputActivity.java
+++ src/com/android/stk/StkInputActivity.java
@@ -81,6 +81,7 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
     private static final String RESPONSE_SENT_KEY = "response_sent";
     private static final String INPUT_STRING_KEY = "input_string";
     private static final String ALARM_TIME_KEY = "alarm_time";
+    private static final String PENDING = "pending";
 
     private static final String INPUT_ALARM_TAG = LOG_TAG;
     private static final long NO_INPUT_ALARM = -1;
@@ -89,6 +90,8 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
     private StkAppService appService = StkAppService.getInstance();
 
     private boolean mIsResponseSent = false;
+    // Determines whether this is in the pending state.
+    private boolean mIsPending = false;
     private int mSlotId = -1;
 
     // Click listener to handle buttons press..
@@ -109,8 +112,8 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
             input = mTextIn.getText().toString();
             break;
         case R.id.button_cancel:
-            appService.getStkContext(mSlotId).setPendingActivityInstance(this);
             sendResponse(StkAppService.RES_ID_END_SESSION);
+            finish();
             return;
         // Yes/No layout buttons.
         case R.id.button_yes:
@@ -143,7 +146,6 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
             break;
         }
         CatLog.d(LOG_TAG, "handleClick, ready to response");
-        appService.getStkContext(mSlotId).setPendingActivityInstance(this);
         sendResponse(StkAppService.RES_ID_INPUT, input, false);
     }
 
@@ -209,6 +211,11 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
         super.onResume();
         CatLog.d(LOG_TAG, "onResume - mIsResponseSent[" + mIsResponseSent +
                 "], slot id: " + mSlotId);
+        // If the terminal has already sent response to the card when this activity is resumed,
+        // keep this as a pending activity as this should be finished when the session ends.
+        if (!mIsResponseSent) {
+            setPendingState(false);
+        }
 
         if (mAlarmTime == NO_INPUT_ALARM) {
             startTimeOut();
@@ -229,8 +236,8 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
         super.onStop();
         CatLog.d(LOG_TAG, "onStop - mIsResponseSent[" + mIsResponseSent + "]");
 
-        // Nothing should be done here if this activity is being restarted now.
-        if (isChangingConfigurations()) {
+        // Nothing should be done here if this activity is being finished or restarted now.
+        if (isFinishing() || isChangingConfigurations()) {
             return;
         }
 
@@ -241,9 +248,9 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
                 finish();
             }
         } else {
-            // This should be set as the pending activity here
+            // This should be registered as the pending activity here
             // only when no response has been sent back to the card.
-            appService.getStkContext(mSlotId).setPendingActivityInstance(this);
+            setPendingState(true);
         }
     }
 
@@ -287,7 +294,6 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
         switch (keyCode) {
         case KeyEvent.KEYCODE_BACK:
             CatLog.d(LOG_TAG, "onKeyDown - KEYCODE_BACK");
-            appService.getStkContext(mSlotId).setPendingActivityInstance(this);
             sendResponse(StkAppService.RES_ID_BACKWARD, null, false);
             return true;
         }
@@ -327,6 +333,11 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
         }
         args.putBoolean(StkAppService.HELP, help);
         startService(new Intent(this, StkAppService.class).putExtras(args));
+
+        // This instance should be set as a pending activity and finished by the service
+        if (resId != StkAppService.RES_ID_END_SESSION) {
+            setPendingState(true);
+        }
     }
 
     @Override
@@ -373,7 +384,6 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
             return true;
         case StkApp.MENU_ID_HELP:
             sendResponse(StkAppService.RES_ID_INPUT, "", true);
-            finish();
             return true;
         }
         return false;
@@ -385,6 +395,7 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
         outState.putBoolean(RESPONSE_SENT_KEY, mIsResponseSent);
         outState.putString(INPUT_STRING_KEY, mTextIn.getText().toString());
         outState.putLong(ALARM_TIME_KEY, mAlarmTime);
+        outState.putBoolean(PENDING, mIsPending);
     }
 
     @Override
@@ -404,6 +415,22 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
         mAlarmTime = savedInstanceState.getLong(ALARM_TIME_KEY, NO_INPUT_ALARM);
         if (mAlarmTime != NO_INPUT_ALARM) {
             startTimeOut();
+        }
+
+        if (!mIsResponseSent && !savedInstanceState.getBoolean(PENDING)) {
+            // If this is in the foreground and no response has been sent to the card,
+            // this must not be registered as pending activity by the previous instance.
+            // No need to renew nor clear pending activity in this case.
+        } else {
+            // Renew the instance of the pending activity.
+            setPendingState(true);
+        }
+    }
+
+    private void setPendingState(boolean on) {
+        if (mIsPending != on) {
+            appService.getStkContext(mSlotId).setPendingActivityInstance(on ? this : null);
+            mIsPending = on;
         }
     }
 
@@ -553,8 +580,6 @@ public class StkInputActivity extends Activity implements View.OnClickListener,
                 public void onAlarm() {
                     CatLog.d(LOG_TAG, "The alarm time is reached");
                     mAlarmTime = NO_INPUT_ALARM;
-                    appService.getStkContext(mSlotId).setPendingActivityInstance(
-                            StkInputActivity.this);
                     sendResponse(StkAppService.RES_ID_TIMEOUT);
                 }
             };
